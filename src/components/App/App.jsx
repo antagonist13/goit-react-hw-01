b12b2ab62c73348a31e4afec693376719171eb6e
antagonist13 <1703shakayaroslaw@gmail.com>
2024-05-26 13:22:36 +0300

    task 2 finish

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,21 +1,21 @@
 import UserData from "/src/userData.json"
+import friends from "/src/friends.json";
 import Profile from "../Profile/Profile";
 import FriendList from "../FriendList/FriendList.jsx";
 import css from "./App.module.css";
-
 
 export default function App() {
     return <div >
         <h1 className={css.header}>React Homework 1</h1>
         <h2>Task 1</h2>
-        <Profile
-        name={UserData.username}
-        tag={UserData.tag}
-        location={UserData.location}
-        image={UserData.avatar}
-        stats={UserData.stats}
-        />
+            <Profile
+                name={UserData.username}
+                tag={UserData.tag}
+                location={UserData.location}
+                image={UserData.avatar}
+                stats={UserData.stats}
+            />
         <h2>Task 2</h2>
-        <FriendList />
+            <FriendList friends={friends} />
     </div>
 }
--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,10 +1,14 @@
-
-export default function FriendList() {
-    return <div >
-        <div>
-            <img src="" alt="Avatar" width="48" />
-            <p>Friend name</p>
-            <p>Friend status</p>
-        </div>
-    </div>
+import FriendListItem from "../FriendListItem/FriendListItem"
+import css from "./FriendList.module.css"
+export default function FriendList({ friends }) {
+    console.log(friends[0].id)
+    return <ul className={css.friendsList}>
+        {/* Кількість li залежить від кількості об'єктів в масиві */}
+        {friends.map(friend => (
+            <li className={css.card} key={friend.id}>
+                <FriendListItem friend={friend} />
+            </li>
+        ))}
+	
+</ul>
 }
